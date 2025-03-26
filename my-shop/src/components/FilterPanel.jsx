a256f76ec5123e75919d2453b88ff0fd5006b297
golikova_elizaveta <golikovaliza385@gmail.com>
2025-03-26 20:47:47 +0300

    first commit

---
+++ my-shop/src/components/FilterPanel.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Button, ButtonGroup } from '@mui/material';
+
+const FilterPanel = ({ onFilterChange }) => {
+    const categories = ['Все', 'electronics', 'clothing', 'books'];
+    return (
+        <ButtonGroup variant="contained" sx={{ my: 2 }}>
+            {categories.map((category) => (
+                <Button key={category} onClick={() => onFilterChange(category)}>
+                    {category}
+                </Button>
+            ))}
+        </ButtonGroup>
+    );
+};
+
+export default FilterPanel;
